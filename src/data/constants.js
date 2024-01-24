2240159567428bd39acad038943a3c630cf220ab
RAJVEER SINGH <syrajveer639@gmail.com>
2024-01-24 21:58:14 +0530

    Resume Link Update

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/47-dev",
   resume:
-    'https://drive.google.com/file/d/1ExwSYfGvQ44G7Hhac3dB3zhbbDw3ossH/view?usp=drive_link',
+    'https://drive.google.com/file/d/1ExwSYfGvQ44G7Hhac3dB3zhbbDw3ossH/view?usp=sharing',
   linkedin: "https://www.linkedin.com/in/rajveer-singh-b037a6203/",
   twitter: "https://twitter.com/syRAJVEER",
   insta: "https://www.instagram.com/syrajveer/",
