8c5906fe4f9220b58633727cda508554401a4906
RAJVEER SINGH <79639577+47-dev@users.noreply.github.com>
2024-03-10 19:46:16 +0530

    Update constants.js
    Profile page changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,8 +2,8 @@ export const Bio = {
   name: "Rajveer Singh",
   roles: [
     "Full Stack Developer",
-    "UI/UX Designer",
-    "Programmer",
+    "React-Devloper",
+    "Backend-Devloper",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
